18367bba8f84138d0b3835f69118aafbc072e230
Johannes <59510166+Joonsey@users.noreply.github.com>
2020-10-06 19:29:54 +0200

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -5,15 +5,21 @@ from secret import token, ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSU
 #import tweepy
 client = commands.Bot(command_prefix='.')
 TOKEN = token
-status = cycle(["With Tor's feelings", 'War Thunder','& Getting Cat Facts'])
+status = cycle(["With Tor's feelings", 'War Thunder','& Getting Cat Facts', 'Jailbait for Jonsey'])
 thingsRasmusSaid = []
 #Twitter auth
 #auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 #auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 #snufkin = tweepy.API(auth)
-
-#f = requests.get('https://api.imgflip.com/get_memes')
+#body = {
+#    "template_id" : 112126428,
+#    "password" : "starcraft",
+#    "username" : "Joonsey",
+#    "text0" : "...",
+#    "text1" : "...",
+#}
+f = requests.get('https://api.imgflip.com/get_memes')
 r = requests.get('https://cat-fact.herokuapp.com/facts')
 #w = requests.get('http://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts&exchars=500&format=json')
 #s = requests.get('eu.api.blizzard.com')
@@ -59,6 +65,92 @@ async def on_ready():
     #print(client.user.id)
     #print(client.users)
 
+class memes(commands.Cog):
+    """A bunch of memes that you can customize with arguments.
+    Arguments are differantiated by ','."""
+    @commands.command(aliases=['memet', 'mt'])
+    async def memetemplate(self, ctx):
+        "random meme template"
+        n = f.json()["data"]["memes"][random.randint(0,len(f.json()["data"]["memes"]))]
+        await ctx.send(n['url'])
+        await ctx.send(n['name'] +", id: " + n['id'])
+
+    @commands.command(aliases=['m1', 'db'])
+    async def meme1(self, ctx, *args):
+        "cheeting boyfriend meme, takes 3 arguments"
+        arg1, arg2, arg3 = " ".join(args).split(",")[0]," ".join(args).split(",")[1], " ".join(args).split(",")[2]
+        await ctx.send(requests.post("https://api.imgflip.com/caption_image", data={
+        "template_id" : 112126428,
+        "password" : "starcraft",
+        "username" : "Joonsey",
+        "boxes[0][text]" : arg1, 
+        "boxes[1][text]" : arg2,
+        "boxes[2][text]": arg3}).json()['data']['url'])
+        await ctx.message.delete()
+
+    @commands.command(aliases=["redbutton","2choices","choices","button", "m2"])
+    async def meme2(self, ctx, *args):
+        "red button, two choices meme, takes 2 arguments"
+        arg1, arg2 = " ".join(args).split(",")[0]," ".join(args).split(",")[1]
+        await ctx.send(requests.post("https://api.imgflip.com/caption_image", data={
+        "template_id" : 87743020,
+        "password" : "starcraft",
+        "username" : "Joonsey",
+        "text0" : arg1,
+        "text1" : arg2}).json()['data']['url'])
+        await ctx.message.delete()
+
+    @commands.command(aliases=["bigbrain","expandingbrain","eb","brain", "m3"])
+    async def meme3(self, ctx, *args):
+        "expanding brain meme, takes 4 arguments"
+        arg1, arg2, arg3, arg4 = " ".join(args).split(",")[0]," ".join(args).split(",")[1], " ".join(args).split(",")[2], " ".join(args).split(",")[3] 
+        await ctx.send(requests.post("https://api.imgflip.com/caption_image", data={
+        "template_id" : 93895088,
+        "password" : "starcraft",
+        "username" : "Joonsey",
+        "boxes[0][text]" : arg1, 
+        "boxes[1][text]" : arg2,
+        "boxes[2][text]" : arg3,
+        "boxes[3][text]" : arg4}).json()['data']['url'])
+        await ctx.message.delete()
+
+    @commands.command(aliases=['slap', "m4"])
+    async def meme4(self, ctx, *args):
+        "batman slapping robbin meme, takes 2 arguments"
+        arg1, arg2 = " ".join(args).split(",")[0]," ".join(args).split(",")[1]
+        await ctx.send(requests.post("https://api.imgflip.com/caption_image", data={
+        "template_id" : 438680,
+        "password" : "starcraft",
+        "username" : "Joonsey",
+        "text0" : arg1,
+        "text1" : arg2}).json()['data']['url'])
+        await ctx.message.delete()
+
+    @commands.command(aliases=['car','drift','m5'])
+    async def meme5(self, ctx, *args):
+        "car drifting hard right meme, takes 3 arguments"
+        arg1, arg2, arg3 = " ".join(args).split(",")[0]," ".join(args).split(",")[1], " ".join(args).split(",")[2]
+        await ctx.send(requests.post("https://api.imgflip.com/caption_image", data={
+        "template_id" : 124822590,
+        "password" : "starcraft",
+        "username" : "Joonsey",
+        "boxes[0][text]" : arg1, 
+        "boxes[1][text]" : arg2,
+        "boxes[2][text]": arg3}).json()['data']['url'])
+        await ctx.message.delete()
+
+    @commands.command(aliases=['m6','cat', "catgirls", 'catwoman'])
+    async def meme6(self, ctx, *args):
+        "2 women mad at cat, takes 2 arguments"
+        arg1, arg2 = " ".join(args).split(",")[0]," ".join(args).split(",")[1]
+        await ctx.send(requests.post("https://api.imgflip.com/caption_image", data={
+        "template_id" : 188390779,
+        "password" : "starcraft",
+        "username" : "Joonsey",
+        "text0" : arg1,
+        "text1" : arg2}).json()['data']['url'])
+        await ctx.message.delete()
+
 @client.command()
 async def ping(ctx):
     "Displays ping"
@@ -87,9 +179,9 @@ async def eyes(ctx):
 
 
 @client.command(aliases=['8ball','Eightball'])
-async def _8ball(ctx,*,arg):
+async def _8ball(ctx,*, arg):
     "Helps you see your future (or past)"
-    await ctx.send(f"""Question: {arg} \nAnswer: {random.choice(['Definatly!','Most likely','Probably','Maybe','with extreme certainty!','No way!'])}""")
+    await ctx.send(f"""Question: {arg} \nAnswer: {random.choice(['Definitely!','Most likely','Probably','Maybe','with extreme certainty!','No way!'])}""")
 
 @tasks.loop(seconds=10)
 async def change_status():
@@ -100,10 +192,12 @@ async def on_message(message):
     if message.author.id == 311125741616234507:
         thingsRasmusSaid.append(str(message.content))
         print("Rasmus just said something! He said: ", message.content)
+    elif message.author.id == 267112523335991307:
+        if "gay" in message.content.lower():
+            await message.channel.send(":eyes:")
     await client.process_commands(message)
-        
     
-@client.command()
+@client.command(aliases=['weather'])
 async def vær(ctx):
     "Shows weather information in MY local area"
     await ctx.send("The temperature is: " + str(værdata["air_temperature"]) + "°C \n" +
@@ -120,11 +214,10 @@ async def shame(ctx):
 
 @client.command()
 async def embed(ctx, *args):
-    embedVar = discord.Embed(title="I have no clue how to use this properly", description="At all", color=0x00ff00)
-    embedVar.add_field(name="Field1", value=" ".join(args), inline=False)
-    embedVar.add_field(name="Field2", value="Whoever used this is an idiot lmao", inline=False)
+    embedVar = discord.Embed(title="I have no clue how to use this properly", description=" ".join(args), color=0x00ff00)
     await ctx.send(embed=embedVar)
-
+    await ctx.message.delete()
+client.add_cog(memes())
 client.run(TOKEN)
 
 #snufkin.update_status(thingsRasmusSaid)
